b6ed0cbf5fae42fb323f7b10417a81a26adf9dc8
rriya1 <75749669+rriya1@users.noreply.github.com>
2026-06-19 02:47:57 +0530

    Update iterative.java

--- dsa/leetcode/00234-palindrome-linked-list/iterative.java
+++ dsa/leetcode/00234-palindrome-linked-list/iterative.java
@@ -10,7 +10,7 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode slow = new ListNode(-1, head);
+        ListNode slow = head;
         ListNode fast = head;
 
         while(fast!=null && fast.next!=null){
